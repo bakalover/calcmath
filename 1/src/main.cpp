77da814199975c1dd149e3d69c5bda278e81ee3c
bakalover <kostya@shprenger.com>
2023-02-20 21:09:19 +0300

    added fixeed error

--- 1/src/main.cpp
+++ 1/src/main.cpp
@@ -1,76 +1,89 @@
-#include<iostream>
+#include <iostream>
 #include <vector>
 #include "../include/util.hpp"
 #include <iomanip>
 #include <fstream>
 using namespace std;
 
-int main(){
-    cout<<setprecision(30)<<fixed;
+int main()
+{
+    cout << setprecision(30) << fixed;
     int32_t n;
     int64_t eps;
     ifstream in;
 
-    cout<<"Choose configuration: \n1 - console \n2 - file\n"<<endl;
+    cout << "Choose configuration: \n1 - console \n2 - file\n"
+         << endl;
     int16_t conf;
-    cin>>conf;
-    if(conf==2){
-        cout<<"Enter path: ";
+    cin >> conf;
+    int i=1;
+    
+    if (conf == 2)
+    {
+        cout << "Enter path: ";
 
         string path;
-        cin>>path;
+        cin >> path;
 
         in.open(path);
-        if(in.is_open()){
-            in>>n;
+        if (in.is_open())
+        {
+            in >> n;
         }
-        else{
-            cout<<"Wrong file"<<endl;;
+        else
+        {
+            cout << "Wrong file" << endl;
+            ;
             return 0;
         }
     }
-    else{
-        cout<<"\nDemensions: ";
-        cin>>n;
+    else
+    {
+        cout << "\nDemensions: ";
+        cin >> n;
     }
 
-    vector<vector<double>> A (n+1,vector<double> (n+1)), C (n+1,vector<double> (n+1));
-    vector<double> B(n+1), d(n+1);
+    vector<vector<double>> A(n + 1, vector<double>(n + 1)), C(n + 1, vector<double>(n + 1));
+    vector<double> B(n + 1), d(n + 1);
 
-    if(conf==2){
+    if (conf == 2)
+    {
         for (size_t i = 1; i <= n; i++)
         {
             for (size_t j = 1; j <= n; j++)
             {
-                in>>A[i][j];
+                in >> A[i][j];
             }
         }
         for (size_t i = 1; i <= n; i++)
         {
-            in>>B[i];
+            in >> B[i];
         }
-        in>>eps;
+        in >> eps;
     }
-    else{
-        cout<<"\nMatrix: "<<endl;;
+    else
+    {
+        cout << "\nMatrix: " << endl;
+        ;
         for (size_t i = 1; i <= n; i++)
         {
             for (size_t j = 1; j <= n; j++)
             {
-                cin>>A[i][j];
+                cin >> A[i][j];
             }
         }
-        cout<<"\nVector: "<<endl;
+        cout << "\nVector: " << endl;
         for (size_t i = 1; i <= n; i++)
         {
-            cin>>B[i];
+            cin >> B[i];
         }
-        cout<<"\nPrecision: ";
-        cin>>eps;
+        cout << "\nPrecision: ";
+        cin >> eps;
     }
 
-    if(!check_diag(A,B)){
-        cout<<"Diagonal dominating is  absent!"<<endl;
+    if (!check_diag(A, B))
+    {
+        cout << "Diagonal dominating is  absent!" << endl;
         return 0;
     }
 
@@ -78,30 +91,31 @@ int main(){
     {
         for (size_t j = 1; j <= n; j++)
         {
-            if(i!=j){
-                C[i][j]=-(A[i][j]/A[i][i]);
+            if (i != j)
+            {
+                C[i][j] = -(A[i][j] / A[i][i]);
             }
         }
-        
     }
 
     for (size_t i = 1; i <= n; i++)
     {
-        d[i] = B[i]/A[i][i];
+        d[i] = B[i] / A[i][i];
     }
-    
-    vector<double> x(d);
-    
-    int32_t iter_count = solve(C,x,d,eps);
 
-    cout<<"Iterations: "<<iter_count<<'\n'<<'\n'<<"Answer: "<<endl;
+    vector<double> x(d);
+
+    int32_t iter_count = solve(C, x, d, eps);
+
+    cout << "Iterations: " << iter_count << '\n'
+         << '\n'
+         << "Answer: " << endl;
     for (size_t i = 1; i < x.size(); i++)
     {
-        cout<<pretty(x[i],5)<<endl;
+        cout << pretty(x[i], 5) << endl;
     }
-    
-    return 0;
 
+    return 0;
 }
 
 // 3
--- 1/src/util.cpp
+++ 1/src/util.cpp
@@ -1,10 +1,18 @@
 #include "../include/util.hpp"
 #include <cmath>
-string pretty(double x, const int64_t eps){
+string pretty(double x, const int64_t eps)
+{
     ostringstream strs;
-    strs << scientific << setprecision(100) << x;
-    string s =  strs.str(); // Просто циклом посчитать нули после этой строчки
-    string mantis = s.substr(0,1) + s.substr(2,eps);
+    strs << fixed << setprecision(100) << x;
+    string s = strs.str(); // Просто циклом посчитать нули после этой строчки
+    string ans;
+    int8_t count{};
+    for (size_t i = 0; count <= 3; ++i)
+    {
+        ans += s[i];
+        s[i] != '0' && i != 0 ? ++count : 0;
+    }
+    /*string mantis = s.substr(0,1) + s.substr(2,eps);
     int16_t exp = stoi(s.substr(s.size()-2,2)) - 1;
     if (exp >= 0){
         for (size_t i = 0; i < exp; i++)
@@ -15,60 +23,68 @@ string pretty(double x, const int64_t eps){
     }
     else{
         return mantis.substr(0,1) + "." + mantis.substr(1, mantis.size()-1);
-    }
+    }*/
+    return ans;
 }
 
-bool check_ans(const vector<double>& x_k, const vector<double>& x_k_1, const double eps, vector<double>& err){
+bool check_ans(const vector<double> &x_k, const vector<double> &x_k_1, const double eps, vector<double> &err)
+{
     double max_cur_err{};
-    for (size_t i = 1; i <= x_k.size()-1; i++)
+    for (size_t i = 1; i <= x_k.size() - 1; i++)
     {
         max_cur_err = max(abs(x_k[i] - x_k_1[i]), max_cur_err);
         err[i] = abs(x_k[i] - x_k_1[i]);
     }
-    if(max_cur_err > eps){
+    if (max_cur_err > eps)
+    {
         return false;
     }
     return true;
 }
 
-void calculate_x_1(vector<double>& x_1, vector<double>& x, const vector<vector<double>>& C, const vector<double>& d){
+void calculate_x_1(vector<double> &x_1, vector<double> &x, const vector<vector<double>> &C, const vector<double> &d)
+{
     for (size_t i = 1; i < x_1.size(); i++)
     {
         x_1[i] = d[i];
-        for (size_t j = 1; j <=(i-1); j++)
+        for (size_t j = 1; j <= (i - 1); j++)
         {
-            x_1[i]+=C[i][j]*x_1[j];
+            x_1[i] += C[i][j] * x_1[j];
         }
-        for (size_t j = i; j <= x_1.size()-1; j++)
+        for (size_t j = i; j <= x_1.size() - 1; j++)
         {
-            x_1[i]+=C[i][j]*x[j];
+            x_1[i] += C[i][j] * x[j];
         }
     }
 }
 
-int32_t solve(const vector<vector<double>>& C, vector<double>& x,const vector<double>& d, const int64_t eps){
-   
+int32_t solve(const vector<vector<double>> &C, vector<double> &x, const vector<double> &d, const int64_t eps)
+{
+
     int32_t iter_count{};
     vector<double> err(x.size());
-    vector<double> x_1(x.size(),100000);
+    vector<double> x_1(x.size(), 100000);
 
-    calculate_x_1(x_1,x,C,d);
+    calculate_x_1(x_1, x, C, d);
 
-    while(!check_ans(x,x_1,(double)1/pow(10,eps),err)){
+    while (!check_ans(x, x_1, (double)1 / pow(10, eps), err))
+    {
         ++iter_count;
         x = x_1;
-        calculate_x_1(x_1,x,C,d);
+        calculate_x_1(x_1, x, C, d);
     }
-    cout<<"\nERR: "<<endl;
-    for(size_t i = 1; i< err.size();++i){
-        //cout<<err[i]<<endl;
+    cout << "\nERR: " << endl;
+    for (size_t i = 1; i < err.size(); ++i)
+    {
+        // cout<<err[i]<<endl;
         cout << pretty(err[i], eps) << endl;
     }
-    cout<<'\n';
+    cout << '\n';
     return ++iter_count;
 }
 
-bool check_diag(vector<vector<double>>& A, vector<double>& b){
+bool check_diag(vector<vector<double>> &A, vector<double> &b)
+{
 
     vector<int32_t> indexes(A.size());
     bool strict = false;
@@ -79,36 +95,43 @@ bool check_diag(vector<vector<double>>& A, vector<double>& b){
         double sum{};
         for (size_t j = 1; j < A.size(); j++)
         {
-            sum+=A[i][j];
+            sum += A[i][j];
         }
-        
+
         for (size_t j = 1; j < A.size(); j++)
         {
-            if(A[i][j] >= sum - A[i][j]){                    //Если такой элемент существует -> он , ((очевидно)), единственнен 
+            if (A[i][j] >= sum - A[i][j])
+            { // Если такой элемент существует -> он , ((очевидно)), единственнен
                 check_row = true;
-                if(A[i][j] > sum - A[i][j]) {strict = true;} // Хотя бы один должен быть строго больше
-                indexes[j] = i;                              // j-й элемент в i-й строке - нужный -> j-ой строкой будет i-я строка
-            }   
+                if (A[i][j] > sum - A[i][j])
+                {
+                    strict = true;
+                }               // Хотя бы один должен быть строго больше
+                indexes[j] = i; // j-й элемент в i-й строке - нужный -> j-ой строкой будет i-я строка
+            }
         }
-        if(!check_row){return false;}
+        if (!check_row)
+        {
+            return false;
+        }
     }
-    for(size_t i = 1; i<indexes.size()-1;++i){
+    for (size_t i = 1; i < indexes.size() - 1; ++i)
+    {
         swap(b[i], b[indexes[i]]);
         for (size_t j = 1; j < A.size(); j++)
         {
-            swap(A[i][j],A[indexes[i]][j]);
+            swap(A[i][j], A[indexes[i]][j]);
         }
-        
     }
     double sum{};
     for (size_t i = 1; i < indexes.size(); i++)
     {
         sum += indexes[i];
     }
-    if(sum - (A.size()-1)*A.size()/2 > 0.000001 || !strict) {
+    if (sum - (A.size() - 1) * A.size() / 2 > 0.000001 || !strict)
+    {
         return false;
     }
-
 
     return true;
 }
