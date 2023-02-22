7c92231d4c43e9cb03de6b997cc80a4ec794b47d
bakalover <kostya@shprenger.com>
2023-02-23 01:44:30 +0300

    architecture

--- 2/eq/src/calc.rs
+++ 2/eq/src/calc.rs
@@ -2,3 +2,27 @@ pub mod bin;
 pub mod newton;
 pub mod newton_multi;
 pub mod simpl_iter;
+
+enum Methods {
+    Bin,
+    Newton,
+    NewtonMulti,
+    Simpl,
+}
+
+pub struct Data{
+    method: Methods,
+    a_0: f64,
+    b_0: f64,
+}
+
+pub fn calculate(data: &Data) -> f64{
+    let mut res: f64;
+    match data.method {
+        Methods::Bin => todo!(),
+        Methods::Newton => todo!(),
+        Methods::NewtonMulti => todo!(),
+        Methods::Simpl => todo!(), 
+    }
+    res
+}
--- 2/eq/src/main.rs
+++ 2/eq/src/main.rs
@@ -1,7 +1,15 @@
+use std::{fs::File, io::{Read, BufReader, BufRead}, error::Error};
+
 
 pub mod draw;
 pub mod calc;
 
 fn main() {
-    crate::draw::draw();
+    /*crate::draw::draw().unwrap();
+    let reader = BufReader::new(File::open("hello.txt").unwrap());
+    let mut x: Vec<i64> = reader.lines().map(|line| line.unwrap().parse::<i64>().unwrap()).collect();
+    print!("{}, {}",x[0],x[1]);
+    let a: Option<i32>;*/
+
+    
 }
