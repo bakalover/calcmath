d80db55ebf9361e4e066a0acd8e1020aba0223b3
bakalover <kostya@shprenger.com>
2023-04-20 23:11:01 +0300

    ANY DEMENSION!!!

--- aprox/aprox/src/main.rs
+++ aprox/aprox/src/main.rs
@@ -2,9 +2,11 @@ use funcs::poly;
 
 fn main() {
     let a:Vec<(f32, f32)> = vec![(1.0,1.0),(1.5,1.5),(5.0,7.0),(8.0,20.0),(-10.0,-3.0)];
-    let linear = match poly(a.as_slice(), funcs::Type::Cube){
+    let linear = match poly(a.as_slice(), 4){
         Ok(val) => val,
         Err(err) =>return,
     };
-    println!("{} {} {} {}",linear[0],linear[1], linear[2], linear[3]); 
+    for i in linear{
+        println!("{}",i);
+    }
 }
