d38a518ee53b50e9fa46892e58446e9d2a2cbbc7
bakalover <kostya@shprenger.com>
2023-04-23 18:09:43 +0300

    Done

--- aprox/aprox/src/main.rs
+++ aprox/aprox/src/main.rs
@@ -1,5 +1,9 @@
-use std::{f32::consts::E, io::stdin};
+use std::{
+    f32::consts::E,
+    io::{stdin, stdout, Write},
+};
 
+use drawing::draw;
 use funcs::{
     exp, log,
     metrics::{get_emp, print_metrics},
@@ -7,15 +11,6 @@ use funcs::{
 };
 
 fn main() {
-    // let a: Vec<(f32, f32)> = vec![
-    //     (1.0, 1.0),
-    //     (1.5, 1.5),
-    //     (5.0, 7.0),
-    //     (8.0, 20.0),
-    //     (-10.0, -3.0),
-    //     (20.0, 30.0),
-    //     (100.0, 200.0),
-    // ];
     println!("Введите число точек N (8 <= N <= 12): ");
 
     let N: usize;
@@ -72,18 +67,24 @@ fn main() {
         Ok(val) => val,
         Err(err) => {
             println!("Невозможно определить линейную функцию: {}", err.0);
-            Vec::new()
+            return;
         }
     };
     println!("\nЛинейная функция: ({})x + ({})", linear[1], linear[0]);
     let linear_metrics = get_emp(&pts, |x| linear[1] * x + linear[0]);
     print_metrics(&linear_metrics);
+    draw(
+        &pts,
+        |x| linear[1] * x + linear[0],
+        "/home/bakalover/code/calcmath/aprox/out/lin.png".to_string(),
+    )
+    .unwrap();
 
     let quad = match poly(pts.as_slice(), 3) {
         Ok(val) => val,
         Err(err) => {
             println!("Невозможно определить квадратичную функцию: {}", err.0);
-            Vec::new()
+            return;
         }
     };
     println!(
@@ -92,12 +93,18 @@ fn main() {
     );
     let quad_metrics = get_emp(&pts, |x| quad[2] * x * x + quad[1] * x + quad[2]);
     print_metrics(&quad_metrics);
+    draw(
+        &pts,
+        |x| quad[2] * x * x + quad[1] * x + quad[2],
+        "/home/bakalover/code/calcmath/aprox/out/quad.png".to_string(),
+    )
+    .unwrap();
 
     let cube = match poly(pts.as_slice(), 4) {
         Ok(val) => val,
         Err(err) => {
             println!("Невозможно определить кубическую функцию: {}", err.0);
-            Vec::new()
+            return;
         }
     };
     println!(
@@ -108,12 +115,18 @@ fn main() {
         cube[3] * x * x * x + cube[2] * x * x + cube[1] * x + cube[0]
     });
     print_metrics(&cube_metrics);
+    draw(
+        &pts,
+        |x| cube[3] * x * x * x + cube[2] * x * x + cube[1] * x + cube[0],
+        "/home/bakalover/code/calcmath/aprox/out/cube.png".to_string(),
+    )
+    .unwrap();
 
     let step = match step(pts.as_slice()) {
         Ok(val) => val,
         Err(err) => {
             println!("Невозможно определить Степенную функцию: {}", err.0);
-            ((0.0, 0.0), (0.0, 0.0))
+            return;
         }
     };
     println!(
@@ -124,12 +137,18 @@ fn main() {
         step.0 .0 * (x - step.1 .0).powf(step.0 .1) - step.1 .1
     });
     print_metrics(&step_metrics);
+    draw(
+        &pts,
+        |x| step.0 .0 * (x - step.1 .0).powf(step.0 .1) - step.1 .1,
+        "/home/bakalover/code/calcmath/aprox/out/step.png".to_string(),
+    )
+    .unwrap();
 
     let exp = match exp(pts.as_slice()) {
         Ok(val) => val,
         Err(err) => {
             println!("Невозможно определить экспоненциальную функцию: {}", err.0);
-            ((0.0, 0.0), 0.0)
+            return;
         }
     };
     println!(
@@ -138,12 +157,18 @@ fn main() {
     );
     let exp_metrics = get_emp(&pts, |x| exp.0 .0 * (E).powf(exp.0 .1 * x) - exp.1);
     print_metrics(&exp_metrics);
+    draw(
+        &pts,
+        |x| exp.0 .0 * (E).powf(exp.0 .1 * x) - exp.1,
+        "/home/bakalover/code/calcmath/aprox/out/exp.png".to_string(),
+    )
+    .unwrap();
 
     let log = match log(pts.as_slice()) {
         Ok(val) => val,
         Err(err) => {
             println!("Невозможно определить Логарифмическую функцию: {}", err.0);
-            ((0.0, 0.0), 0.0)
+            return;
         }
     };
     println!(
@@ -152,8 +177,41 @@ fn main() {
     );
     let log_metrics = get_emp(&pts, |x| log.0 .1 * (x - log.1).ln() - log.0 .0);
     print_metrics(&log_metrics);
+    draw(
+        &pts,
+        |x| log.0 .1 * (x - log.1).ln() - log.0 .0,
+        "/home/bakalover/code/calcmath/aprox/out/log.png".to_string(),
+    )
+    .unwrap();
 
-    // println!("{} {} {} ", linear.0 .0, linear.0 .1, linear.1);
+    let mut metrics: Vec<(f32, usize)> = Vec::new();
+    metrics.push((linear_metrics.1, 0));
+    metrics.push((quad_metrics.1, 1));
+    metrics.push((cube_metrics.1, 2));
+    metrics.push((step_metrics.1, 3));
+    metrics.push((exp_metrics.1, 4));
+    metrics.push((log_metrics.1, 5));
+
+    let mut min_metric = (f32::MAX, 5);
+    for el in metrics {
+        if (el.0 < min_metric.0) {
+            min_metric = el;
+        }
+    }
+
+    println!("  ");
+    print!("Лучшая аппроксимация: ");
+    stdout().flush();
+
+    match min_metric.1 {
+        0 => println!("Линейная"),
+        1 => println!("Квадратичная"),
+        2 => println!("Кубическая"),
+        3 => println!("Степенная"),
+        4 => println!("Экспоненциальная"),
+        5 => println!("Логарифмическая"),
+        _ => panic!("aaaaaa"),
+    }
 }
 // 1 1
 // 1.5 1.5
@@ -164,4 +222,21 @@ fn main() {
 // 100 2
 // 5 5
 // 6 6
-//
+
+// 1 1
+// 2 2
+// 3 3
+// 4 4
+// 5 5
+// 6 6
+// 7 7
+// 8 8
+
+// 1 1
+// 1 1
+// 1 1
+// 1 1
+// 1 1
+// 1 1
+// 1 1
+// 1 1
